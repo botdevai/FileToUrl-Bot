852123b9913b04f0e155f1c5168b098db7dcb6a7
botdevai <73134281+botdevai@users.noreply.github.com>
2020-12-21 14:44:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ if Port != 80:
 
 # These example values won't work. You must get your own api_id and
 # api_hash from https://my.telegram.org, under API Development.
-api_id = int(os.environ.get("id", ""))
+api_id = 1834507
 api_hash = os.environ.get("hash", "")
 bot_token = os.environ.get("TG_BOT_TOKEN", "")
 
